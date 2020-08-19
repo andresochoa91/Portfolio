c7a2346c6e4ac27f6d51e65810513eda0316d907
andresochoa91 <andres_ochoa91@hotmail.com>
2020-08-18 23:52:17 -0700

    things added in react router

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -6,20 +6,18 @@ import Projects from './Components/Projects';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 import "tachyons";
-import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Route } from 'react-router-dom';
 // import Profile from './Profile'; 
 // import { Todo } from './types'; //When using types.ts 
 
 const App: React.FC = () => {
   return (
     <>
-      <Router>
-        <Header />
-        <Route exact path="/" render={() => <About />} />  
-        <Route exact path="/profile" render={() => <Profile />} />  
-        <Route exact path="/projects" render={() => <Projects />} />  
-        <Route exact path="/contact" render={() => <Contact />} />        
-      </Router>
+      <Header />
+      <Route exact path="/" render={() => <About />} />  
+      <Route exact path="/profile" render={() => <Profile />} />  
+      <Route exact path="/projects" render={() => <Projects />} />  
+      <Route exact path="/contact" render={() => <Contact />} />        
       <Footer />
     </>
   );
--- portfolio/src/Components/Contact.tsx
+++ portfolio/src/Components/Contact.tsx
@@ -13,11 +13,6 @@ const Contact: React.FC = () => {
         <p>San Francisco. CA. 94116</p>
         <p>(415) 996-86-72</p>
       </div>
-      <p className="lh-copy measure center f6 black-70">
-        Quite affectionate and outgoing.
-        She loves to get chin scratches and will
-        roll around on the floor waiting for you give her more of them.
-      </p>
       <footer className="pv4 ph3 tc">
         <a className="link hover-silver near-black dib mh3 tc" href="https://www.linkedin.com/in/jandresochoa91/" title="LinkedIn">
           <svg className="dib h2 w2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill-rule="evenodd" clip-rule="evenodd" stroke-linejoin="round" stroke-miterlimit="1.414"><path d="M13.632 13.635h-2.37V9.922c0-.886-.018-2.025-1.234-2.025-1.235 0-1.424.964-1.424 1.96v3.778h-2.37V6H8.51V7.04h.03c.318-.6 1.092-1.233 2.247-1.233 2.4 0 2.845 1.58 2.845 3.637v4.188zM3.558 4.955c-.762 0-1.376-.617-1.376-1.377 0-.758.614-1.375 1.376-1.375.76 0 1.376.617 1.376 1.375 0 .76-.617 1.377-1.376 1.377zm1.188 8.68H2.37V6h2.376v7.635zM14.816 0H1.18C.528 0 0 .516 0 1.153v13.694C0 15.484.528 16 1.18 16h13.635c.652 0 1.185-.516 1.185-1.153V1.153C16 .516 15.467 0 14.815 0z" fill-rule="nonzero"/></svg>
