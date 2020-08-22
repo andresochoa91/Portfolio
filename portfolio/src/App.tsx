7e70d45f420bb204d194c9bda4994d1229fe9371
andresochoa91 <andres_ochoa91@hotmail.com>
2020-08-21 23:49:49 -0700

    bugs fixed, ready to refactoring

--- portfolio/src/App.tsx
+++ portfolio/src/App.tsx
@@ -1,4 +1,4 @@
-import React/* , { useState } */ from 'react';
+import React, { useEffect, useState } from 'react';
 import "tachyons";
 import Header from './Components/Header';
 import About from './Components/About';
@@ -7,17 +7,55 @@ import Projects from './Components/Projects';
 import Contact from './Components/Contact';
 import Footer from './Components/Footer';
 import { Route, Switch, Redirect } from 'react-router-dom';
-// import Profile from './Profile'; 
-// import { Todo } from './types'; //When using types.ts 
+
+
 
 const App: React.FC = () => {
+  const [ studies, setStudies ] = useState<Array<Studies>>([]);
+  const [ experience, setExperience ] = useState<Array<Experience>>([]);
+  const [ skills, setSkills ] = useState<Array<Skills>>([]);
+  const apiKey: string = "keyBeXZyPUF8I8G0u"
+
+
+  useEffect(() => {
+    fetch(`https://api.airtable.com/v0/appkh2au2vrFCqrR6/Table%201?api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      setStudies(data.records.map((study: any) => {
+        return {...study.fields, id: study.id};  
+      }));
+    })
+
+    fetch(`https://api.airtable.com/v0/appkh2au2vrFCqrR6/Table%202?api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      setExperience(data.records.map((exp: any) => {
+        return {...exp.fields, id: exp.id};  
+      }));
+    });
+
+    fetch(`https://api.airtable.com/v0/appkh2au2vrFCqrR6/Table%203?api_key=${apiKey}`)
+    .then(response => response.json())
+    .then(data => {
+      setSkills(data.records.map((sk: any) => {
+        return {...sk.fields, id: sk.id};  
+      }));
+    });
+  }, []);
+
   return (
     <>
       <Header />
       <Switch>
         <Route exact path="/" render={() => <Redirect to="/about" />} />  
         <Route path="/about" render={() => <About />} />
-        <Route path="/profile" render={(props) => <Profile {...props}/>} />  
+        <Route path="/profile" render={(props) => 
+          <Profile 
+            studies={ studies } 
+            experience={ experience } 
+            skills={ skills }
+            {...props}/>}
+        />  
         <Route path="/projects" render={() => <Projects />} />  
         <Route path="/contact" render={() => <Contact />} />        
       </Switch>
--- portfolio/src/Components/Profile/Education.tsx
+++ portfolio/src/Components/Profile/Education.tsx
@@ -1,61 +1,35 @@
 import React from 'react';
-import codeTheDream from '../../Images/codeTheDream.png';
-import codeTenderloin from '../../Images/codeTenderloin.png';
-import unaula from '../../Images/unaula.png';
 
-const Education: React.SFC = () => {
+interface IProps {
+  studies: Array<Studies>;
+}
+
+const Education: React.SFC<IProps> = ({ studies }) => {
   return (
     <section className="mw7 mt4 center">
-      <article className="pv4 bb b--black-10 ph3 ph0-l">
-        <div className="flex flex-column flex-row-ns">
-          <div className="w-100 w-60-ns pr3-ns order-2 order-1-ns">
-            <h1 className="f3 athelas mt0 lh-title">Code the Dream</h1>
-            <h3 className="athelas">Computer Software Engineering</h3>
-            <p className="f5 f4-l lh-copy athelas">
-              I passed the bootcamp test for Code The Dream and I was admitted into the advanced High Noon program where we learn Full-Stack development.
-            </p>
-          </div>
-          <div className="pl3-ns order-1 order-2-ns mb4 mb0-ns w-100 w-40-ns">
-            <img src={ codeTheDream } className="db" alt="" />
-          </div>
-        </div>
-        <p className="f6 lh-copy gray mv0"><span className="ttu">In progress</span></p>
-      </article>
-
-      <article className="pv4 bb b--black-10 ph3 ph0-l">
-        <div className="flex flex-column flex-row-ns">
-          <div className="w-100 w-60-ns pr3-ns order-2 order-1-ns">
-            <h1 className="f3 athelas mt0 lh-title">Code Tenderloin</h1>
-            <h3 className="athelas">Computer Software Engineering</h3>
-            <p className="f5 f4-l lh-copy athelas">
-              Completed Code Tenderloin’s Job Readiness Program, Code Ramp program and the advanced Code Ramp ++ programs. 
-            </p>
-          </div>
-          <div className="pl3-ns order-1 order-2-ns mb4 mb0-ns w-100 w-40-ns">
-            <img src={ codeTenderloin } className="db" alt="" />
-          </div>
-        </div>
-        <p className="f6 lh-copy gray mv0"><span className="ttu">Class of 2020</span></p>
-      </article>
-
-      <article className="pv4 bb b--black-10 ph3 ph0-l">
-        <div className="flex flex-column flex-row-ns">
-          <div className="w-100 w-60-ns pr3-ns order-2 order-1-ns">
-            <h1 className="f3 athelas mt0 lh-title">Industrial Engineering</h1>
-            <h3 className="athelas">Latin American Autonomous University</h3>
-            <p className="f5 f4-l lh-copy athelas">
-              Industrial Engineer with a minor in Logistics.
-            </p>
-            <p className="f5 f4-l lh-copy athelas">
-              Active member of the "Ingeco" research group, participating in national and international meetings in Medellín, Cartagena and Bucaramanga, on the impact of engineering on national development.
-            </p>
-          </div>
-          <div className="pl3-ns order-1 order-2-ns mb4 mb0-ns w-100 w-40-ns">
-            <img src={ unaula } className="db w-70" alt="" />
-          </div>
-        </div>
-        <p className="f6 lh-copy gray mv0">By <span className="ttu">Class of 2015</span></p>
-      </article>
+      {
+        studies.map((st: Studies) => (
+          <article key={ st.id } className="pv4 bb b--black-10 ph3 ph0-l">
+            <div className="flex flex-column flex-row-ns">
+              <div className="w-100 w-60-ns pr3-ns order-2 order-1-ns">
+                <h1 className="f3 athelas mt0 lh-title">{st.school}</h1>
+                <h3 className="athelas">{st.title}</h3>
+                {
+                  JSON.parse(st.description).map((desc: string, i: number) => (
+                    <p key={ `${desc}${i}` } className="f5 f4-l lh-copy athelas">
+                      {desc}
+                    </p>
+                  ))
+                }
+              </div>
+              <div className="pl3-ns order-1 order-2-ns mb4 mb0-ns w-100 w-40-ns">
+                <img src={ st.image } className="db" alt="" />
+              </div>
+            </div>
+            <p className="f6 lh-copy gray mv0"><span className="ttu">{st.status}</span></p>
+          </article>      
+        ))
+      }
     </section>
 
 
