37891c9bd40daff15f874dcf38a97fffbc9698c1
andresochoa91 <andres_ochoa91@hotmail.com>
2020-09-15 15:19:47 -0700

    something

---
+++ build/precache-manifest.a196902bfda5dbfbb0855e91c54fe8dc.js
@@ -0,0 +1,54 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f1b5bc5a8043b18bfb48a820ee2c8efe",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a6533c028c690fffc868",
+    "url": "/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "bfbd3aca5e33a7b556f2",
+    "url": "/static/css/main.f6c87854.chunk.css"
+  },
+  {
+    "revision": "a6533c028c690fffc868",
+    "url": "/static/js/2.5fffc7a1.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.5fffc7a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bfbd3aca5e33a7b556f2",
+    "url": "/static/js/main.e82608d0.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "50231cd27ed1d067f76e7c6ac3f5fa99",
+    "url": "/static/media/cityWeather.50231cd2.png"
+  },
+  {
+    "revision": "a1717a865d817e6156d810cf3e4c108b",
+    "url": "/static/media/faceRecognition.a1717a86.png"
+  },
+  {
+    "revision": "fc5540c1c77e6418b4a2648f3adf91df",
+    "url": "/static/media/languageIcon.fc5540c1.png"
+  },
+  {
+    "revision": "a101c4ff5058943dadc0e2bed4513bfd",
+    "url": "/static/media/profilePicture.a101c4ff.jpg"
+  },
+  {
+    "revision": "ebf062e16138d76d7dd50d567059dffa",
+    "url": "/static/media/ticTacToe.ebf062e1.png"
+  },
+  {
+    "revision": "b79d8b2dc8c87843bfeec36e46680825",
+    "url": "/static/media/ufoInSF.b79d8b2d.png"
+  }
+]);
